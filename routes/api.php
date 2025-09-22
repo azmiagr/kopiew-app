47f3ebb591b202caf59a74e50bc917011f0a6786
Sebastian <sebastiannoel7106@gmail.com>
2025-09-22 21:30:36 +0700

    feat: add CRUD endpoints for Place

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PlaceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,10 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/places', [PlaceController::class, 'store']);
+    Route::delete('/place/{place}', [PlaceController::class, 'destroy']);
+    Route::put('/place/{place}', [PlaceController::class, 'update']);
 });
+
+Route::get('/places', [PlaceController::class, 'index']);
+Route::get('/place/{place}', [PlaceController::class, 'show']);
