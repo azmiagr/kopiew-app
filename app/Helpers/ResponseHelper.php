b4a5e6b88f5f905c900725b4e1289cf86b73c91a
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-09-23 23:51:07 +0700

    feat: add ResponseHelper

---
+++ app/Helpers/ResponseHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+class ResponseHelper
+{
+    public static function success($data = null, $message = 'Success', $code = 200)
+    {
+        return response()->json([
+            'status' => [
+                'code' => $code,
+                'isSuccess' => true
+            ],
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    public static function error($message = 'Error', $code = 400, $errors =  null)
+    {
+        $response = [
+            'status' => [
+                'code' => $code,
+                'isSuccess' => false
+            ],
+            'message' => $message,
+        ];
+
+        if ($errors) {
+            $response['errors'] = $errors;
+        }
+
+        return response()->json($response, $code);
+    }
+}
