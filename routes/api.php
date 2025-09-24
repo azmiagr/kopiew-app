5cece52f2c7d3f5ba7b9fac1b1fe3bb54296a996
Hafid580 <hafidfirdaus580@gmail.com>
2025-09-24 11:03:05 +0700

    feat: Menambahkan fitur CRUD wishlist

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,20 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\WishlistController;
 
 Route::post('/daftar', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+
+Route::get('/wishlists', [WishlistController::class, 'index']);
+Route::get('/wishlists/{wishlist}', [WishlistController::class, 'show']);
+Route::post('/wishlists', [WishlistController::class, 'store']);
+Route::put('/wishlists/{wishlist}', [WishlistController::class, 'update']);
+Route::patch('/wishlists/{wishlist}', [WishlistController::class, 'update']);
+Route::delete('/wishlists/{wishlist}', [WishlistController::class, 'destroy']);
+
 });
