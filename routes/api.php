8a1d1e54e88c51cbabf986fe624e1226f2c92fd1
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-09-28 14:41:30 +0700

    feat: add routes for review

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,24 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/daftar', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/reviews', [ReviewController::class, 'index']);
+Route::get('/place/{place}/reviews', [ReviewController::class, 'show']);
+
+
 
 Route::middleware('auth:api')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index']);
+    Route::put('/profile', [ProfileController::class, 'update']);
+    Route::post('/place/{place}/reviews', [ReviewController::class, 'store']);
+    Route::put('/place/{place}/reviews/{review}', [ReviewController::class, 'update']);
+    Route::delete('/place/{place}/reviews/{review}', [ReviewController::class, 'destroy']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
